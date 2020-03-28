9ed1659d7458529f910063924d4dc446e08590e4
yongsheng.ma <yongsheng.ma@dingxiang-inc.com>
2020-03-28 14:09:19 +0800

    排序算法

---
+++ web/src/main/java/com/ysma/ppt/ali/ShowMeBug.java
@@ -0,0 +1,121 @@
+package com.ysma.ppt.ali;// ## 请在下方描述你的面试题内容( 支持Markdown )
+
+import java.io.*;
+import java.util.*;
+
+public class ShowMeBug {
+  /*
+   * 输入两个升序(从小到大)的整数数列，合并成一个升序(从小到大)的数列返回。
+   * 要求不能用系统提供的sort，并尽量考虑优化算法效率。
+   * 例如：输入 [1,3,5] , [2,4,6] 输入 [1,2,3,4,5,6]
+   */
+  public static ArrayList<Integer> mergeArray(ArrayList<Integer> arr1, ArrayList<Integer> arr2) {
+    // 在这里写代码
+    arr1.addAll(arr2);
+    fastSort(arr1, 0, arr1.size()-1);
+    return arr1;
+  }
+
+  public static void fastSort(ArrayList<Integer> arr, int low, int high){
+    int start = low;
+    int end = high;
+    int key = arr.get(low);
+    while (start < end){
+      //后->前
+      while (end > start && arr.get(end) >= key){
+        end--;
+      }
+      if(arr.get(end) <= key){//swap
+        Integer temp = arr.get(end);
+        arr.set(end, arr.get(start));
+        arr.set(start, temp);
+      }
+
+      //前->后
+      while (start < end && arr.get(start) <= key){
+        start++;
+      }
+      if(arr.get(start) >= key){//swap
+        Integer temp = arr.get(start);
+        arr.set(start, arr.get(end));
+        arr.set(end, temp);
+      }
+
+      //递归
+      if(start>low) fastSort(arr, low, start-1);
+      if(end<high) fastSort(arr, end+1, high);
+    }
+  }
+
+  //插入排序
+  public static ArrayList<Integer> mysort(ArrayList<Integer> arr){
+    for(int i =1; i < arr.size(); i++){
+      for(int j = 0; j < i; j++){
+        if (arr.get(j) > arr.get(i)){
+          Integer temp = arr.get(j);
+          arr.set(j, arr.get(i));
+          arr.set(i, temp);
+        }
+      }
+    }
+    return arr;
+  }
+  
+  /*
+   * 请添加一个测试实例
+   */
+  public static void test3() {
+    // 在这里写测试
+
+  }
+  
+  public static void main(String[] args) {
+    test1();
+    //test2();
+    //test3();
+  }
+
+  /*
+   * 测试合并[1,3,5],[2,4,6]，期望输出[1,2,3,4,5,6]
+   */  
+  public static void test1() {
+    ArrayList<Integer> arr1 = new ArrayList<Integer>();
+    arr1.add(1);
+    arr1.add(3);
+    arr1.add(5);
+
+    ArrayList<Integer> arr2 = new ArrayList<Integer>();
+    arr2.add(2);
+    arr2.add(4);
+    arr2.add(6);
+    
+    ArrayList<Integer> result = mergeArray(arr1, arr2);
+    for (Integer num : result) {
+      System.out.print(num +",");
+    } // 期望输出 1 2 3 4 5 6    
+  }
+
+  /*
+   * 测试合并[-10,10,100],[1,2,3,4,200,300,400]，期望输出[-10,1,2,3,4,10,100,200,300,400]
+   */  
+  public static void test2() {
+    ArrayList<Integer> arr1 = new ArrayList<Integer>();
+    arr1.add(-10);
+    arr1.add(10);
+    arr1.add(100);
+
+    ArrayList<Integer> arr2 = new ArrayList<Integer>();
+    arr2.add(1);
+    arr2.add(2);
+    arr2.add(3);
+    arr2.add(4);
+    arr2.add(200);
+    arr2.add(300);
+    arr2.add(400);
+    
+    ArrayList<Integer> result = mergeArray(arr1, arr2);
+    for (Integer num : result) {
+      System.out.println(num);
+    } // 期望输出 [-10,1,2,3,4,10,100,200,300,400]
+  }
+}
--- web/src/test/java/com/ysma/ppt/Test.java
+++ web/src/test/java/com/ysma/ppt/Test.java
@@ -1,90 +1,32 @@
 package com.ysma.ppt;
 
-import com.alibaba.fastjson.JSON;
-import com.google.common.collect.ClassToInstanceMap;
-import com.google.common.collect.MutableClassToInstanceMap;
-import lombok.Data;
 
 import java.util.*;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) {
-        /*List<Long> policyLabs = Arrays.asList(1L,2L,34L,56L,78L);
-        List<Integer> labIds = policyLabs.stream()
-                .mapToInt(Long::intValue)
-                .collect(ArrayList::new, List::add, List::addAll);
-        System.out.println(labIds);*/
-        List<String> policyLabs = Arrays.asList("1","2","34","56","78");
-        String a = String.join(",", policyLabs);
-        System.out.println(a);
+        Student student = new Student();
+        student.hello();
 
     }
-    public static void main5(String[] args) {
-        List<Person> list = new ArrayList<>();
-        list.addAll(Arrays.asList(new Person("ysma", 18),
-                new Person("malian", 20),
-                new Person("zsy",18)));
-        /*Map<String, Person> map = list.stream().collect(Collectors.toMap(Person::getName, v->v, (v1, v2)->v2));
-        System.out.println(JSON.toJSONString(map));*/
-        Person a = new Person("ysma", 21);
-        Person b = new Person("bella", 18);
-        List<Person> listb = Arrays.asList(a, b);
-        for(Person temp : listb){
-            list.removeIf(f->f.getName().equals(temp.getName()));
-            if (list.stream().noneMatch((f) -> f.getName().equals(temp.getName()))){
-                list.add(temp);
-            }
-        }
-        System.out.println(JSON.toJSONString(list));
 
-
-    }
-    public static void main4(String[] args) throws InterruptedException {
-        BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10000);
-        queue.offer(1);
-        Integer val;
-        while ((val = queue.poll()) != null){
-
-            System.out.println(val);
-        }
-        /*for(int i =0; i< 58; i++)
-            queue.offer(i);
-
-        int times = (queue.size() + 9)/10;
-        for(int i =0; i< times; i++){
-            List<Integer> list = new ArrayList<>(10);
-            queue.drainTo(list, 10);
-            System.out.println(JSON.toJSONString(list));
-        }*/
-    }
-    public static void main3(String[] args) {
-        ClassToInstanceMap<Integer> classToInstanceMap = MutableClassToInstanceMap.create();
-        classToInstanceMap.put(Integer.class, 1024);
-        classToInstanceMap.compute(Integer.class, (aClass, integer) -> {
-            if(integer != null){
-                System.out.println("old val:" + integer);
-                return integer + 1;
-            }
-            return 1026;
-        });
-        System.out.println(classToInstanceMap.get(Integer.class));
-    }
-
-    @Data
-    static class Person{
+    static abstract class Person{
         private String name;
         private int age;
 
-        public Person(String name, int age) {
-            this.name = name;
-            this.age = age;
+
+        void hello(){
+            say("test");
         }
 
-        public Person() {
+        void say(String input){
+            System.out.println("===>" + input);
         }
+    }
 
+    static class Student extends Person{
+        void say(String input){
+            System.out.println("test:===>" + input);
+        }
     }
 }
