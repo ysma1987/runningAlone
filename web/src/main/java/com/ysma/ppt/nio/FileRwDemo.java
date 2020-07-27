fa4c067332731317ae2612a68c02e00625edb748
mayongsheng331@163.com <ysma101506>
2020-07-27 15:07:13 +0800

    文件读写demo

--- web/src/main/java/com/ysma/ppt/nio/FileRwDemo.java
+++ web/src/main/java/com/ysma/ppt/nio/FileRwDemo.java
@@ -22,8 +22,9 @@ public class FileRwDemo {
     private static final String outPath2 = "D:\\ysma\\_copy2.txt";
 
     public static void main(String[] args) {
-        nioVersion();
-        ioVersion();
+        /*nioVersion();
+        ioVersion();*/
+        nioVersion2();
     }
 
     /**
@@ -46,6 +47,9 @@ public class FileRwDemo {
 
     /**
      * nio版本
+     * transferTo复制出来的文件，大小只有2G
+     *   因为：int var8 = (int)Math.min(var3, 2147483647L);
+     * transferFrom复制出来的文件，大小同原文件
      */
     private static void nioVersion() {
         FileChannel in = null, out = null;
@@ -61,5 +65,22 @@ public class FileRwDemo {
         }
     }
 
+    /**
+     * 解除transferTo复制的2G限制
+     * 零拷贝应用
+     */
+    private static void nioVersion2(){
+        FileChannel in = null, out = null;
+        try {
+            in = FileChannel.open(Paths.get(inPath), StandardOpenOption.READ);
+            out = FileChannel.open(Paths.get(outPath), StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+            long size = in.size();
+            out.transferFrom(in, 0, size);
+        } catch (IOException ignore) {
+        } finally {
+            IOUtils.closeQuietly(out);
+            IOUtils.closeQuietly(in);
+        }
+    }
 
 }
